643fea03e5a01a99673bbdd56d9bdd43b2dd6d8b
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-02-10 11:15:02 -0800

    made almost no changes, but broadcast for each client

--- src/ws/v1/WebSocketServer.ts
+++ src/ws/v1/WebSocketServer.ts
@@ -1,9 +1,21 @@
-import { Server } from "ws";
+import WebSocket, { Server } from "ws";
 
 export class WebSocketServer {
 	public wss: Server;
 	public constructor(public port: number) {}
 	public start(): void {
 		this.wss = new Server({ port: this.port });
+	}
+
+	public connection(): void {
+		this.wss.on("connection", (ws) => {
+			ws.on("message", (message) => {
+				this.wss.clients.forEach((client) => {
+					if (client.readyState === WebSocket.OPEN) {
+						client.send(message);
+					}
+				});
+			});
+		});
 	}
 }
