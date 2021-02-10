5c98029b2d4ca6a36c10f4593369fefa61b0bc98
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-02-10 10:34:42 -0800

    add axios

--- src/api/v1/routes/auth/auth.controller.ts
+++ src/api/v1/routes/auth/auth.controller.ts
@@ -1,11 +1,16 @@
 import { Request, Response } from "express";
+import axios from "axios";
 
 export class AuthController {
 	public github(req: Request, res: Response): void {
-		//github oauth code
+		res.redirect(
+			`https://github.com/login/oauth/authorize?client_id=${process.env.GH_CLIENT_ID}`
+		);
 	}
 
 	public githubCallback(req: Request, res: Response): void {
-		//github oauth callback code
+		const code = req.query.code;
+
+		axios;
 	}
 }
