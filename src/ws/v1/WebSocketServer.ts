34af2cea3768dcde84daf53a31304a23d04af5b5
Milo123459 <milo5huntington@gmail.com>
2021-02-11 16:50:15 +0000

    feat(Wss): add pinging

--- src/ws/v1/WebSocketServer.ts
+++ src/ws/v1/WebSocketServer.ts
@@ -1,14 +1,25 @@
 import WebSocket, { Server } from "ws";
 
+interface BetterSocket extends WebSocket {
+	isAlive?: boolean;
+}
 export class WebSocketServer {
 	public wss: Server;
 	public constructor(public port: number) {}
 	public start(): void {
 		this.wss = new Server({ port: this.port });
+		this.connection();
 	}
 
 	public connection(): void {
-		this.wss.on("connection", (ws) => {
+		function noop() {}
+
+		function heartbeat() {
+			this.isAlive = true;
+		}
+		this.wss.on("connection", (ws: BetterSocket) => {
+			ws.isAlive = true;
+			ws.on("pong", heartbeat);
 			ws.on("message", (message) => {
 				this.wss.clients.forEach((client) => {
 					if (client.readyState === WebSocket.OPEN) {
@@ -16,6 +27,18 @@ export class WebSocketServer {
 					}
 				});
 			});
+		});
+		const interval = setInterval(function ping() {
+			this.wss.clients.forEach(function each(ws: BetterSocket) {
+				if (ws.isAlive == false) return ws.terminate();
+
+				ws.isAlive = false;
+				ws.ping(noop);
+			});
+		}, 30000);
+
+		this.wss.on("close", function close() {
+			clearInterval(interval);
 		});
 	}
 }
